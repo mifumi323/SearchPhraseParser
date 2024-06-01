03368f78c132fc4a2e1598fa46536482108e313d
mifumi323 <mifumi323@gmail.com>
2024-06-01 23:36:47 +0900

    互換性用のクラス

---
+++ old/Parser.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace MifuminLib\SearchPhraseParser;
+
+/**
+ * @deprecated 3.0.0 use \Mifumi323\SearchPhraseParser\Parser
+ */
+class Parser extends \Mifumi323\SearchPhraseParser\Parser
+{
+}
